4a6264faa6457e34e8e6fdf50e54b470e7a23e07
Yuriy <last1992@list.ru>
2018-07-17 08:33:03 +0300

    fix length

--- app/models/Post.rb
+++ app/models/Post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
 
   validates :title, presence: true
-  validates :text, presence: true, length: { in: 6..254 }
+  validates :text, presence: true, length: { in: 1..254 }
 end
